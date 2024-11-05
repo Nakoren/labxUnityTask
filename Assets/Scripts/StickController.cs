47edd975ab6f388cceebaffd6e3729843f18fcda
Nakoren <minenkov_vladimir2003@inbox.ru>
2024-11-05 15:08:08 +0300

    FIxes
    Fixed stick rotation, collision also removed unnecessary scripts

--- Assets/Scripts/StickController.cs
+++ Assets/Scripts/StickController.cs
@@ -6,7 +6,6 @@ using UnityEngine;
 public class StickController : MonoBehaviour
 {
     public StickScript stick;
-    private Vector3 baseRotation;
     public float maxAngle = 30;
     public float speed = 1f * 200;
 
